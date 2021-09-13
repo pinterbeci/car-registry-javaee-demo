35e55ddc296c2c0c7da7b619d85f5ddd13325271
pinterbeci <pinter.beci99@gmail.com>
2021-09-13 21:24:56 +0200

    CRUD ready.

--- car-registry-frontend/src/main/java/hu/pinterbeci/java/course/maven/rest/CoreRestService.java
+++
@@ -1,9 +0,0 @@
-package hu.pinterbeci.java.course.maven.rest;
-
-import hu.pinterbeci.java.course.maven.entity.AbstractCar;
-import hu.pinterbeci.java.course.maven.rest.model.CoreRestModel;
-
-public abstract class CoreRestService<T extends AbstractCar, M extends CoreRestModel> {
-
-
-}
